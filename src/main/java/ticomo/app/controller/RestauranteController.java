7a9de387d04e71c57adc4dd4e7dc714d93a166b7
Uve95 <MVictoria.Alcazar@alu.uclm.es>
2022-11-06 18:19:26 +0100

    Update RestauranteController.java

--- src/main/java/ticomo/app/controller/RestauranteController.java
+++ src/main/java/ticomo/app/controller/RestauranteController.java
@@ -2,7 +2,7 @@ package ticomo.app.controller;
 
 import java.util.Optional;
 
-
+    
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
